54b722588073cb81eb0271afd323cb5adad96b36
AndyEJ <64099169+AndreaEJ22@users.noreply.github.com>
2022-10-25 07:39:18 -0500

    Yeah

---
+++ codejana_flask/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_brcrypt import Bcrypt
+
+app=Flask(__name__)
+
+app.config['SECRET_KEY']='ef1f56we61fwe51cwe51c51wc6ewc5'
+#app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database/cjflask.db'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://cjpostgres:cjflask@localhost:5435/cjflaskapp'
+
+db = SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+from codejana_flask import routes
+
+
+
---
+++ codejana_flask/routes.py
@@ -0,0 +1,48 @@
+
+
+from codejana_flask import app, db
+from flask import Flask, render_template, url_for, redirect, flash
+from codejana_flask.forms import RegistrationForm,LoginForm
+from codejana_flask.models import User
+
+@app.route('/')
+@app.route('/home')
+def homepage():
+    return render_template('homepage.html', title='Home')
+
+
+
+@app.route('/about')
+def about():
+    return render_template('About.html', title='About')
+
+
+@app.route('/account')
+def account():
+    return render_template('Account.html', title='Account')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        user=User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created successfully for {form.username.data}', category='success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        if user and form.password.data==user.password:
+            flash(f'Login successful for {form.email.data}', category='success')
+
+            return redirect(url_for('account'))
+        else:
+            flash(f'Login unsuccessful for {form.email.data}', category='danger')
+    return render_template('login.html', title='Login', form=form)
+
