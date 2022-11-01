2292200f85ca0e7539a4ffc8458a4566564e67ba
AndyEJ <64099169+AndreaEJ22@users.noreply.github.com>
2022-11-01 07:24:33 -0600

    UwU Onichan

--- codejana_flask/models.py
+++ codejana_flask/models.py
@@ -1,8 +1,18 @@
 
-from codejana_flask import db
+from codejana_flask import db,login_manager
 from datetime import datetime
+from flask_login import UserMixin
+from flask import redirect,url_for
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
+
+@login_manager.unauthorized_handler
+def unauthorized():
+  return redirect(url_for('register'))
+
+class User(db.Model,UserMixin):
     id=db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(20), unique=True, nullable=False)
     email=db.Column(db.String(120),unique=True,nullable=False)
--- codejana_flask/routes.py
+++ codejana_flask/routes.py
@@ -1,10 +1,12 @@
 
 
 from click import password_option
+from flask_login import login_user
 from codejana_flask import app, db, bcrypt
 from flask import Flask, render_template, url_for, redirect, flash
 from codejana_flask.forms import RegistrationForm,LoginForm
 from codejana_flask.models import User
+from flask_login import login_user,logout_user,current_user,login_required
 
 @app.route('/')
 @app.route('/home')
@@ -25,10 +27,13 @@ def account():
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
+  if current_user.is_authenticated:
+    return redirect(url_for('account'))
+
     form=RegistrationForm()
     if form.validate_on_submit():
         encrypted_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user=User(username=form.username.data, email=form.email.data, password=form.password.data)
+        user=User(username=form.username.data,email=form.email.data,password=encrypted_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Account created successfully for {form.username.data}', category='success')
@@ -37,17 +42,25 @@ def register():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    form=LoginForm()
-    if form.validate_on_submit():
+  if current_user.is_authenticated:
+    return redirect(url_for('account'))
+  form=LoginForm()
+  if form.validate_on_submit():
         #encrypted_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         #user=User(username=form.username.date,email=form.email.data,password=encrypted_password)
-        user=User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password,form.password.data):
+    user=User.query.filter_by(email=form.email.data).first()
         #if user and form.password.data==user.password:
-            flash(f'Login successful for {form.email.data}', category='success')
+    if user and bcrypt.check_password_hash(user.password,form.password.data):
+        login_user(user)
+        flash(f'Login successful for {form.email.data}', category='success')
 
-            return redirect(url_for('account'))
-        else:
-            flash(f'Login unsuccessful for {form.email.data}', category='danger')
-    return render_template('login.html', title='Login', form=form)
+        return redirect(url_for('account'))
+    else:
+        flash(f'Login unsuccessful for {form.email.data}', category='danger')
+  return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
