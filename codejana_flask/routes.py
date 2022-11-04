53c7c30c0f18f689862db309fc75df365254d6cd
AndyEJ <64099169+AndreaEJ22@users.noreply.github.com>
2022-11-04 09:30:58 -0600

    YEAAAAH FUNCIONA PERO SIN LA PAGINA WEB

--- codejana_flask/routes.py
+++ codejana_flask/routes.py
@@ -32,12 +32,12 @@ def register():
 
   form=RegistrationForm()
   if form.validate_on_submit():
-      encrypted_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-      user=User(username=form.username.data,email=form.email.data,password=encrypted_password)
-      db.session.add(user)
-      db.session.commit()
-      flash(f'Account created successfully for {form.username.data}', category='success')
-      return redirect(url_for('login'))
+    encrypted_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+    user=User(username=form.username.data,email=form.email.data,password=encrypted_password)
+    db.session.add(user)
+    db.session.commit()
+    flash(f'Account created successfully for {form.username.data}', category='success')
+    return redirect(url_for('login'))
   return render_template('register.html', title='Register', form=form)
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -51,11 +51,11 @@ def login():
     user=User.query.filter_by(email=form.email.data).first()
         #if user and form.password.data==user.password:
     if user and bcrypt.check_password_hash(user.password,form.password.data):
-        login_user(user)
-        flash(f'Login successful for {form.email.data}', category='success')
-        return redirect(url_for('account'))
+      login_user(user)
+      flash(f'Login successful for {form.email.data}', category='success')
+      return redirect(url_for('account'))
     else:
-        flash(f'Login unsuccessful for {form.email.data}', category='danger')
+      flash(f'Login unsuccessful for {form.email.data}', category='danger')
   return render_template('login.html', title='Login', form=form)
 
 @app.route('/logout')
