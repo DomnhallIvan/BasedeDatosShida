ecb9ff7189ae6b62367e024b0023c145927681e0
AndyEJ <64099169+AndreaEJ22@users.noreply.github.com>
2022-11-07 08:02:54 -0600

    ALFIN

--- codejana_flask/routes.py
+++ codejana_flask/routes.py
@@ -61,7 +61,7 @@ def register():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
   if current_user.is_authenticated:
-    return redirect(url_for('Account'))
+    return redirect(url_for('account'))
   form=LoginForm()
   if form.validate_on_submit():
         #encrypted_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -71,7 +71,7 @@ def login():
     if user and bcrypt.check_password_hash(user.password,form.password.data):
       login_user(user)
       flash(f'Login successful for {form.email.data}', category='success')
-      return redirect(url_for('Account'))
+      return redirect(url_for('account'))
     else:
       flash(f'Login unsuccessful for {form.email.data}', category='danger')
   return render_template('login.html', title='Login', form=form)
